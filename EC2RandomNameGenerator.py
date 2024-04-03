78445e18c5cdca24ced1f00d53155da20cb6c27f
Mohammad Sabahat Khan <sabahatkhan@live.ca>
2024-04-02 22:06:35 -0400

    Added IF statement to verify departments

--- EC2RandomNameGenerator.py
+++ EC2RandomNameGenerator.py
@@ -1,17 +1,26 @@
 import random
 
+#Ask user for input
 numOfEc2 = int(input('Enter the amount of EC2 instances that you need names for: '))
-department = input('Please enter your department: ')
+#print('Please enter one of the following departments Marketing, Accounting, FinOps:')
+department = input('Please enter an eligible department (i.e. Marketing, Accounting, FinOps):')
 
-#Loop through a number of times as specified in numOfEc2
-for x in range(numOfEc2):
-    randomName = ''
-    randomChoiceSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    randomChars = random.choice(randomChoiceSet)
 
-    #Generate 5 random characters
-    for i in range(5):
-        randomChars+=random.choice(randomChoiceSet)
+if (department.lower() == 'marketing') or (department.lower() == 'accounting') or (department.lower() == 'finops'):
+    #Loop through a number of times as specified in numOfEc2
+    for x in range(numOfEc2):
+        randomName = ''
+        randomChoiceSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+        randomChars = random.choice(randomChoiceSet)
 
-    randomName = department + randomChars
-    print(randomName)
+        #Generate 5 random characters
+        for i in range(5):
+            randomChars+=random.choice(randomChoiceSet)
+
+        randomName = department + randomChars
+        if x == 0:
+            print()
+            print('List of names:')
+        print(randomName)
+else:
+    print('The department you have entered is not eligible to use this Name Generator.')
